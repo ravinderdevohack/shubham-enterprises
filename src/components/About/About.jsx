6530e1daef94c143b9f7a3bd9e666c405b74af2b
ravinder <ravinderchauhan@gmail.com>
2026-01-02 20:12:54 +0530

    updated imgae

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
               <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                   <div className="md:5/12 lg:w-5/12">
                       <img
-                          src="../src/assets/images/betonka-4784341_1280.jpg"
+                          src="/images/betonka-4784341_1280.jpg"
                           alt="image"
                       />
                   </div>
