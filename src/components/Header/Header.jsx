023ba7837cf5098211cdfd7af3fad3872bb2973b
ravinder <ravinderchauhan@gmail.com>
2026-01-05 18:53:42 +0530

    created product component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -46,11 +46,20 @@ export default function Header() {
               </li>
               <li>
                 <NavLink
-                  to="/equipment"
+                  to="/equipments"
                   className={({isActive}) =>
                     `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700":"text-gray-800"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 `
                   }>
                   Equipments
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/products"
+                  className={({isActive}) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700":"text-gray-800"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 `
+                  }>
+                  Products
                 </NavLink>
               </li>
               <li>
---
+++ src/components/Products/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Product() {
+
+  return (
+    <div>
+        <div className="flex flex-row justify-around mt-2 mb-8  place-items-center sm:mt-20">
+          <img className="sm:w-96 w-48 rounded-lg" src="https://images.pexels.com/photos/532079/pexels-photo-532079.jpeg" alt="image2" />
+          <img className="sm:w-96 w-48 rounded-lg" src="https://images.pexels.com/photos/532079/pexels-photo-532079.jpeg" alt="image2" />
+        </div>
+      
+    </div>
+  )
+}
